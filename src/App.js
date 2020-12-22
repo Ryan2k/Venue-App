e187a3db48c3ce453765aa3d4863a3830e88bf67
Caroline Freer <cfreer@cs.washington.edu>
2020-12-22 14:30:45 -0800

    starting to add upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import Spinner from './Spinner'
+import Images from './Images'
+import Buttons from './Buttons'
+import { API_URL } from './config'
+import './App.css'
 import uw from './uw.jpg';
+require('dotenv').config()
 
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            uploading: false,
+            images: []
         };
 
         this.handleUpload = this.handleUpload.bind(this);
@@ -25,8 +33,49 @@ class App extends Component {
         let element = document.createElement("a");
         element.click();
     };
+    onChange = e => {
+        const files = Array.from(e.target.files)
+        this.setState({ uploading: true })
+
+        const formData = new FormData()
+
+        files.forEach((file, i) => {
+            formData.append(i, file)
+        })
+
+        fetch(`${API_URL}/image-upload`, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(images => {
+                this.setState({
+                    uploading: false,
+                    images
+                })
+            })
+    }
+
+    removeImage = id => {
+        this.setState({
+            images: this.state.images.filter(image => image.public_id !== id)
+        })
+    }
+
 
     render() {
+        const { uploading, images } = this.state
+
+        const content = () => {
+            switch (true) {
+                case uploading:
+                    return <Spinner/>
+                case images.length > 0:
+                    return <Images images={images} removeImage={this.removeImage}/>
+                default:
+                    return <Buttons onChange={this.onChange}/>
+            }
+        }
         return (
             <div align="center">
                 <button onClick={() => {
@@ -43,6 +92,9 @@ class App extends Component {
                     <button><font size="4">Download</font></button>
                 </a>
                 <br/>
+                <div className='buttons'>
+                    {content()}
+                </div>
             </div>
         );
     }
